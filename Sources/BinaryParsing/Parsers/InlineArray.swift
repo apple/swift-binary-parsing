bc36146e61efde6ebc2f3272f7a97878598ce703
Nate Cook <natecook@apple.com>
2026-01-07 10:53:03 -0600

    Use typed throws for InlineArray parsers

--- Sources/BinaryParsing/Parsers/InlineArray.swift
+++ Sources/BinaryParsing/Parsers/InlineArray.swift
@@ -18,7 +18,7 @@ extension InlineArray where Element == UInt8 {
   /// - Throws: A `ParsingError` if `input` does not have at least `count`
   ///   bytes remaining.
   @inlinable
-  public init(parsing input: inout ParserSpan) throws {
+  public init(parsing input: inout ParserSpan) throws(ParsingError) {
     let slice = try input._divide(atByteOffset: Self.count)
     self = unsafe slice.withUnsafeBytes { buffer in
       InlineArray { unsafe buffer[$0] }
@@ -28,6 +28,7 @@ extension InlineArray where Element == UInt8 {
 
 @available(macOS 26, iOS 26, watchOS 26, tvOS 26, visionOS 26, *)
 extension InlineArray where Element: ~Copyable {
+  #if !$Embedded
   /// Creates a new array by parsing the specified number of elements from the given
   /// parser span, using the provided closure for parsing.
   ///
@@ -57,6 +58,40 @@ extension InlineArray where Element: ~Copyable {
     parser: (inout ParserSpan) throws -> Element
   ) throws {
     self = try InlineArray { _ in
+      try parser(&input)
+    }
+  }
+  #endif
+  
+  /// Creates a new array by parsing the specified number of elements from the given
+  /// parser span, using the provided closure for parsing (with a typed error).
+  ///
+  /// The provided closure is called `count` times while initializing the inline array.
+  /// For example, the following code parses a 16-element `InlineArray` of `UInt32`
+  /// values from a `ParserSpan`. If the `input` parser span doesn't represent enough
+  /// memory for those 16 values, the call will throw a `ParsingError`.
+  ///
+  ///     let integers = try InlineArray<16, UInt32>(parsing: &input) { input in
+  ///         try UInt32(parsingBigEndian: &input)
+  ///     }
+  ///
+  /// You can also pass a parser initializer to this initializer as a value, if it has
+  /// the correct shape:
+  ///
+  ///     let integers = try InlineArray<16, UInt32>(
+  ///         parsing: &input,
+  ///         parser: UInt32.init(parsingBigEndian:))
+  ///
+  /// - Parameters:
+  ///   - input: The `ParserSpan` to consume.
+  ///   - parser: A closure that parses each element from `input`.
+  /// - Throws: An error if one is thrown from `parser`.
+  @inlinable
+  public init(
+    parsing input: inout ParserSpan,
+    parser: (inout ParserSpan) throws(ParsingError) -> Element
+  ) throws(ParsingError) {
+    self = try InlineArray { _ throws(ParsingError) in
       try parser(&input)
     }
   }
